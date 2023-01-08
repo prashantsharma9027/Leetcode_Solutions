d11bc502334e22384d4d28128daf4c033431424b
Prashant Sharma <prashanttonusharma@gmail.com>
2023-01-08 23:00:54 +0530

    Detect Capital

---
+++ 520.detect-capital.cpp
@@ -0,0 +1,31 @@
+#include<bits/stc++>
+/*
+ * @lc app=leetcode id=520 lang=cpp
+ *
+ * [520] Detect Capital
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+        int n = word.size();
+
+        int count = 0;
+
+        for(int i = 0 ; i< n ; i++)
+        {
+            if(isupper(word[i]))
+                count++;    
+        }
+
+        if(count == n || count == 0 || count==1 && isupper(word[0]))
+        {
+            return true;
+        }
+
+        return false;
+    }
+};
+// @lc code=end
+
