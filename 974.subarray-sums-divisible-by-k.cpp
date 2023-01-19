f235f0d1e3fd7d33c6b5b5443960f45b392cf958
Prashant Sharma <prashanttonusharma@gmail.com>
2023-01-20 03:21:36 +0530

    19-01-23

---
+++ 974.subarray-sums-divisible-by-k.cpp
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode id=974 lang=cpp
+ *
+ * [974] Subarray Sums Divisible by K
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int subarraysDivByK(vector<int>& nums, int k) {
+         int n = nums.size();
+        vector<int> sums(k, 0);
+        sums[0]++;
+        int cnt = 0;
+        int currSum = 0;
+        for(int i = 0; i<n; i++) {
+            currSum = (currSum + nums[i]%k + k)%k;
+            cnt += sums[currSum];
+            sums[currSum]++;
+        }
+        return cnt;
+        
+    }
+};
+// @lc code=end
+
