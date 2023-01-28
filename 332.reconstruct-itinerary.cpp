27603d905737e4aa087445b470b1ff68d6acf7aa
Prashant Sharma <prashanttonusharma@gmail.com>
2023-01-28 23:06:08 +0530

    28-01-23

---
+++ 332.reconstruct-itinerary.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=332 lang=cpp
+ *
+ * [332] Reconstruct Itinerary
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> findItinerary(vector<vector<string>>& tickets) {
+        
+    }
+};
+// @lc code=end
+
--- 472.concatenated-words.cpp
+++ 472.concatenated-words.cpp
@@ -31,6 +31,6 @@ public:
         }
         return res;
     }
-};
+};332
 // @lc code=end
 
