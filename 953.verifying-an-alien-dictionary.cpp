410584ea16e443221613d1e368c533e2c2727f13
Prashant Sharma <prashanttonusharma@gmail.com>
2023-02-02 22:53:59 +0530

    02-02-23

---
+++ 953.verifying-an-alien-dictionary.cpp
@@ -0,0 +1,45 @@
+/*
+ * @lc app=leetcode id=953 lang=cpp
+ *
+ * [953] Verifying an Alien Dictionary
+ */
+
+// @lc code=start
+class Solution {
+public:
+     unordered_map<char, int> map;
+    bool check(string word1, string word2){
+    int n= min(word1.length(), word2.length());
+        for(int i=0; i<n; i++){
+            if(map[word1[i]]<map[word2[i]]) return true;
+            else if(map[word1[i]]>map[word2[i]]) return false;
+            
+        }
+    
+        if(word1.size()>word2.size())return false;
+        return true;
+    
+    }
+    
+    
+    
+    
+    bool isAlienSorted(vector<string>& words, string order) {
+       
+    for(int i=0; i< order.length(); i++){
+        map[order[i]]= i+1;
+    } 
+        
+    for(int i=0; i< words.size()-1; i++)    
+    {
+      bool result= check(words[i], words[i+1]);
+        if(result==false) return false;
+        
+        
+    }
+        return true;
+        
+    }
+};
+// @lc code=end
+
