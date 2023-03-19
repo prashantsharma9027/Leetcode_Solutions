a289f86437012bd4dc21054ca2babc38259acd7e
Prashant Sharma <prashanttonusharma@gmail.com>
2023-03-20 04:48:50 +0530

    20-03-23

---
+++ 1622.max-value-of-equation.cpp
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode id=1728 lang=cpp
+ *
+ * [1622] Fancy Sequence
+ */
+
+// @lc code=start
+class Fancy {
+public:
+    Fancy() {
+        
+    }
+    
+    void append(int val) {
+        
+    }
+    
+    void addAll(int inc) {
+        
+    }
+    
+    void multAll(int m) {
+        
+    }
+    
+    int getIndex(int idx) {
+        
+    }
+};
+
+/**
+ * Your Fancy object will be instantiated and called as such:
+ * Fancy* obj = new Fancy();
+ * obj->append(val);
+ * obj->addAll(inc);
+ * obj->multAll(m);
+ * int param_4 = obj->getIndex(idx);
+ */
+// @lc code=end
+
---
+++ 860.design-circular-queue.cpp
@@ -0,0 +1,105 @@
+/*
+ * @lc app=leetcode id=890 lang=cpp
+ *
+ * [860] Lemonade Change
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+        
+    }
+};
+// @lc code=end
+
+lass MyCircularQueue {
+    int rear, front;
+    int size;
+    int *arr;
+public:
+    MyCircularQueue(int k) {
+        front = rear = -1;
+        size = k;
+        arr = new int[k];
+    }
+    
+    bool enQueue(int value) {
+        if ((front == 0 && rear == size-1) ||(rear == (front-1)%(size)))
+        {
+            return false;
+        }
+    
+        else if (front == -1) 
+        {
+            front = rear = 0;
+            arr[rear] = value;
+        }
+    
+        else if (rear == size-1 && front != 0)
+        {
+            rear = 0;
+            arr[rear] = value;
+        }
+    
+        else
+        {
+            rear++;
+            arr[rear] = value;
+        }
+
+        return true;
+        
+    }
+    
+    bool deQueue() {
+        if (front == -1)
+        {
+            return false;
+        }
+    
+        int data = arr[front];
+        arr[front] = -1;
+        if (front == rear)
+        {
+            front = -1;
+            rear = -1;
+        }
+        else if (front == size-1)
+            front = 0;
+        else
+            front++;
+    
+        return true;
+    }
+    
+    int Front() {
+        if(isEmpty())
+            return -1;
+        else
+        return arr[front];
+    }
+    
+    int Rear() {
+        if(isEmpty())
+            return -1;
+        else
+        return arr[rear];
+    }
+  
+    bool isEmpty() {
+        if (front == -1)
+        {
+            return true;
+        }
+        return false;
+    }
+    
+    bool isFull() {
+        if ((front == 0 && rear == size-1) ||(rear == (front-1)%(size)))
+        {
+            return true;
+        }
+        return false;
+    }
+};
