9c9fc9086e126d2b112a65623420f588443483c2
Prashant Sharma <prashanttonusharma@gmail.com>
2023-04-02 01:46:18 +0530

    02-04-23

---
+++ 1704.special-positions-in-a-binary-matrix.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=1823 lang=cpp
+ *
+ * [1704] Determine if String Halves Are Alike
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool halvesAreAlike(string s) {
+        
+    }
+};
+// @lc code=end
+
---
+++ 792.binary-search.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=808 lang=cpp
+ *
+ * [792] Number of Matching Subsequences
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int numMatchingSubseq(string s, vector<string>& words) {
+        
+    }
+};
+// @lc code=end
+
