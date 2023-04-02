d8526955ed254b746b55c541f279503d8d232442
Prashant Sharma <prashanttonusharma@gmail.com>
2023-04-03 00:08:33 +0530

    03-04-23

---
+++ 2200.find-all-k-distant-indices-in-an-array.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2200 lang=cpp
+ *
+ * [2200] Find All K-Distant Indices in an Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> findKDistantIndices(vector<int>& nums, int key, int k) {
+        waw
+    }
+};
+// @lc code=end
+
---
+++ 2300.successful-pairs-of-spells-and-potions.cpp
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode id=2300 lang=cpp
+ *
+ * [2300] Successful Pairs of Spells and Potions
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<int> successfulPairs(vector<int>& spells, vector<int>& potions, long long success) {
+        
+    }
+};
+// @lc code=end
+
