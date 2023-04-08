4f71c8dd397a442f548d251739d08a3f6ea703b3
Prashant Sharma <prashanttonusharma@gmail.com>
2023-04-08 09:12:41 +0530

    08-04-23

---
+++ 222.count-complete-tree-nodes.cpp
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode id=222 lang=cpp
+ *
+ * [222] Count Complete Tree Nodes
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void inorder(TreeNode* root , int &count)
+    {
+        if(root == NULL)
+        {
+            return ;
+        }
+
+        inorder(root->left , count);
+        count++;
+        inorder(root->right , count);
+    }
+    int countNodes(TreeNode* root) {
+        int count = 0;
+        inorder(root , count);
+
+        return count;
+        
+    }
+};
+// @lc code=end
+
