f8aeb494fea3a3776f77832d2c26dcef44fc2888
Prashant Sharma <prashanttonusharma@gmail.com>
2023-04-11 19:38:36 +0530

    11-04-23

---
+++ 2390.removing-stars-from-a-string.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=2390 lang=cpp
+ *
+ * [2390] Removing Stars From a String
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string removeStars(string s) {
+        stack<char> st;
+
+        for (char c : s) {
+            if (c == '*' && !st.empty()) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+
+        string ans = "";
+        while (!st.empty()) {
+            ans += st.top();
+            st.pop();
+        }
+
+        reverse(ans.begin(), ans.end());
+        return ans;
+        
+    }
+};
+// @lc code=end
+
