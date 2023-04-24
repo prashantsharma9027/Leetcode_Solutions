4a2d007b9c9ace1acd03a20594e625238075806e
Prashant Sharma <prashanttonusharma@gmail.com>
2023-04-24 11:14:44 +0530

    24-04-23

--- 1046.last-stone-weight.cpp
+++ 1046.last-stone-weight.cpp
@@ -9,33 +9,48 @@ class Solution {
 public:
     int lastStoneWeight(vector<int>& stones) {
         
-        sort(stones.begin(),stones.end());
+        // sort(stones.begin(),stones.end());
                 
-        while(stones.size() > 1)
-        {
-            int n = stones.size();
-            int n1 = stones[n-1];
-            stones.pop_back();
-            int n2 = stones[n-2];
-            stones.pop_back();
+        // while(stones.size() > 1)
+        // {
+        //     int n = stones.size();
+        //     int n1 = stones[n-1];
+        //     stones.pop_back();
+        //     int n2 = stones[n-2];
+        //     stones.pop_back();
 
-            if(n1!=n2)
-            {
-                int temp = abs(n2-n1);
-                stones.push_back(temp);
-                sort(stones.begin(),stones.end());
-            }
+        //     if(n1!=n2)
+        //     {
+        //         int temp = abs(n2-n1);
+        //         stones.push_back(temp);
+        //         sort(stones.begin(),stones.end());
+        //     }
             
-        }
+        // }
 
-        if(stones.size() == 0)
+        // if(stones.size() == 0)
+        // {
+        //     return 0;
+        // }
+        // else
+        // {
+        //     return stones[0];
+        // }
+
+        priority_queue<int>pq(a.begin(),a.end());
+
+        while(pq.size() > 1)
         {
-            return 0;
+            int a = pq.top();
+            pq.pop();
+            int b = pq.top();
+            pq.pop();
+
+            if(a != b)
+                pq.push(abs(a-b));
         }
-        else
-        {
-            return stones[0];
-        }
+        
+        return pq.empty() ? 0 : pq.top();
 
     }
 };
