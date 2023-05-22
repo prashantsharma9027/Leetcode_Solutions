1db1e8d240acbb6175b6009286c9bcdc8fca1a4d
Prashant Sharma <prashanttonusharma@gmail.com>
2023-05-22 20:23:13 +0530

    22-05-23

--- 347.top-k-frequent-elements.cpp
+++ 347.top-k-frequent-elements.cpp
@@ -29,6 +29,30 @@ public:
         for (int i = 0; i < k; i++) result.push_back(frequency[i].first);
 
         return result;
+
+        // int n=nums.size();
+        // unordered_map<int,int>mp;
+        // vector<int> Res;
+       
+        
+        // for(int i=0;i<n;i++)
+        // {
+        //     mp[nums[i]]++;
+        // }
+        
+        // priority_queue<pair<int,int>> m;
+        // for( auto i : mp)
+        // {
+        //     m.push(make_pair( i.second, i.first));
+        // }
+        
+        // for( int i =0;i<k ; i++)
+        // {
+        //     Res.push_back( m.top().second);
+        //     m.pop();
+        // }
+      
+        // return Res;
     }
 };
 // @lc code=end
