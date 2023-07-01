2ddc424e7d3215f69dd842bd75b6d4601917bf6c
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-07-01 23:49:50 +0530

    01-07-23

---
+++ 746.min-cost-climbing-stairs.cpp
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode id=746 lang=cpp
+ *
+ * [746] Min Cost Climbing Stairs
+ */
+
+// @lc code=start
+class Solution {
+public: 
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n = cost.size();
+        int prev1 = cost[0];
+        int prev2 = cost[1];
+        int curr;
+        
+        for (int i = 2; i < n; i++) 
+        {
+            curr = cost[i] + min(prev1, prev2);
+            prev1 = prev2;
+            prev2 = curr;
+        }
+        
+        return min(prev1, prev2);
+    }
+};
+// @lc code=end
+
