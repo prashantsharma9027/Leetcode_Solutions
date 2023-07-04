a1ba4233717a278be7c1c8321079269082496b6d
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-07-04 08:40:37 +0530

    04-07-23

---
+++ 137.single-number-ii.cpp
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode id=137 lang=cpp
+ *
+ * [137] Single Number II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        map<int,int> mp;
+
+        for(int i = 0 ; i<nums.size() ; i++)
+        {
+            mp[nums[i]]++;
+        }        
+
+        for(auto i : mp)
+        {
+            if(i.second == 1)
+                return i.first;
+        }
+
+        return -1;
+    }
+};
+// @lc code=end
+
