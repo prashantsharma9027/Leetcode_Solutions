445ae13b8174becf6b9ba5e73e6f457ada890002
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-10-10 23:57:12 +0530

    hello

---
+++ 2009.minimum-number-of-operations-to-make-array-continuous.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=2009 lang=cpp
+ *
+ * [2009] Minimum Number of Operations to Make Array Continuous
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        
+        set<int> st(begin(nums), end(nums));
+        
+        vector<int> temp(begin(st), end(st));
+        
+        int result = INT_MAX;
+        
+        for(int i = 0; i<temp.size(); i++) {
+            
+            int left_range  = temp[i];
+            int right_range = left_range + n - 1;
+            
+            int j = upper_bound(begin(temp), end(temp), right_range) - begin(temp);
+            
+            int in_range  = j - i;
+            int out_range = n - in_range;
+            
+            result = min(result, out_range);
+            
+        }
+        
+        return result;
+        
+    }
+};
+// @lc code=end
+
