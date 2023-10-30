04974d2caf4054deb83a59dcc456403fcd21a92f
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-10-30 23:35:56 +0530

    xx

---
+++ 1356.sort-integers-by-the-number-of-1-bits.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=1356 lang=cpp
+ *
+ * [1356] Sort Integers by The Number of 1 Bits
+ */
+
+// @lc code=start
+class Solution {
+public:
+    static bool compare(int a, int b) {
+       
+        int bitCountA = __builtin_popcount(a);
+        int bitCountB = __builtin_popcount(b);
+
+        if (bitCountA == bitCountB) {
+            return a < b;
+        }
+
+        return bitCountA < bitCountB;
+    }
+    
+    vector<int> sortByBits(vector<int>& arr) {
+       
+        sort(arr.begin(), arr.end(), compare);
+        
+        return arr;
+    }
+};
+// @lc code=end
+
