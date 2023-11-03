28d2daf394b3b9143035e399f3eee98638ef8a30
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-11-03 23:11:09 +0530

    03-11-23

---
+++ 1441.build-an-array-with-stack-operations.cpp
@@ -0,0 +1,38 @@
+/*
+ * @lc app=leetcode id=1441 lang=cpp
+ *
+ * [1441] Build an Array With Stack Operations
+ */
+
+// @lc code=start
+class Solution {
+public:
+    vector<string> buildArray(vector<int>& target, int n) {
+        vector<string> operations;
+        
+        int targetIndex = 0;
+        
+        for (int i = 1; i <= n; i++) 
+        {
+            if (targetIndex == target.size()) 
+            {
+                break;  
+            }
+            
+            if (target[targetIndex] == i) 
+            {
+                operations.push_back("Push");
+                targetIndex++;
+            } 
+            else 
+            {
+                operations.push_back("Push");
+                operations.push_back("Pop");
+            }
+        }
+        
+        return operations;
+    }
+};
+// @lc code=end
+
