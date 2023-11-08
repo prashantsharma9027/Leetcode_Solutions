f9f98e6894e2fda5fa2fc995ec95605a998e19f2
prashantsharma9027 <prashanttonusharma@gmail.com>
2023-11-08 22:17:56 +0530

    -07-11-23-

--- 1441.build-an-array-with-stack-operations.cpp
+++ 1441.build-an-array-with-stack-operations.cpp
@@ -25,6 +25,7 @@ public:
                 targetIndex++;
             } 
             else 
+            
             {
                 operations.push_back("Push");
                 operations.push_back("Pop");
