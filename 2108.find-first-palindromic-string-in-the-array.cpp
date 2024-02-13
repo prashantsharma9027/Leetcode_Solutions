7dab9837224cb8faeb0fe1179a4b116ca6ec0f5b
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-02-13 22:42:29 +0530

    op

---
+++ 2108.find-first-palindromic-string-in-the-array.cpp
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode id=2108 lang=cpp
+ *
+ * [2108] Find First Palindromic String in the Array
+ */
+
+// @lc code=start
+class Solution {
+public:
+    string firstPalindrome(vector<string>& words) {
+
+        string ans = "";
+
+        for(auto i : words)
+        {
+            string temp = i;
+            reverse(temp.begin(), temp.end());
+
+            if(temp == i )
+            {
+                return i;
+            } 
+        }
+
+        return "";
+        
+    }
+};
+// @lc code=end
+
