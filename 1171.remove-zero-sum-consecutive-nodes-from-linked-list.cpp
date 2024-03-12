3d601e0a05b380b4d2240ed1a19f22f480e4f322
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-03-12 22:36:19 +0530

    rwe

--- 1171.remove-zero-sum-consecutive-nodes-from-linked-list.cpp
+++ 1171.remove-zero-sum-consecutive-nodes-from-linked-list.cpp
@@ -9,7 +9,6 @@
  * Definition for singly-linked list.
  * struct ListNode {
  *     int val;
- * 
  *     ListNode *next;
  *     ListNode() : val(0), next(nullptr) {}
  *     ListNode(int x) : val(x), next(nullptr) {}
