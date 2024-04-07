f053b737b1caaa9e804bff6f3efe8a1001bdbb24
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-04-08 00:10:11 +0530

    dsa

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -8,24 +8,29 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        int n = nums.size();
+        unordered_map<int, int> numIndexMap;
+        for (int i = 0; i < nums.size(); ++i) 
+        {
+            numIndexMap[nums[i]] = i;
+        }
+        
         sort(nums.begin(), nums.end());
-        int left = 0 ;
-        int right = n - 1;
+        
+        int left = 0;
+        int right = nums.size() - 1;
 
-        while (left < right)
+        while (left < right) 
         {
             int sum = nums[left] + nums[right];
-            if (sum == target)
+            if (sum == target) 
             {
-                return {left, right}; 
-            }
-            else if (sum < target)
+                return {numIndexMap[nums[left]], numIndexMap[nums[right]]};
+            } 
+            else if (sum < target) 
             {
                 left++;
-            }
-            else
-            {
+            } 
+            else {
                 right--;
             }
         }
@@ -33,6 +38,7 @@ public:
         return {-1, -1}; 
     }
 };
+
 
 // @lc code=end
 
