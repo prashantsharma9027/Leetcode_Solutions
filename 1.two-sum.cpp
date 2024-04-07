5557eb53d61b7499f34d49357160972ac50da3f0
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-04-08 00:07:16 +0530

    dsa

--- 1.two-sum.cpp
+++ 1.two-sum.cpp
@@ -7,9 +7,32 @@
 // @lc code=start
 class Solution {
 public:
-     vector<int> twoSum(vector<int>& nums, int target) {
-        
+    vector<int> twoSum(vector<int>& nums, int target) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int left = 0 ;
+        int right = n - 1;
+
+        while (left < right)
+        {
+            int sum = nums[left] + nums[right];
+            if (sum == target)
+            {
+                return {left, right}; 
+            }
+            else if (sum < target)
+            {
+                left++;
+            }
+            else
+            {
+                right--;
+            }
+        }
+
+        return {-1, -1}; 
     }
 };
+
 // @lc code=end
 
