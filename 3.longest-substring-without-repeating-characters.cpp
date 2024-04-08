290a7eb4a8e102d37c2b63cb44eeded92686cdda
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-04-08 23:42:30 +0530

    dsa

--- 3.longest-substring-without-repeating-characters.cpp
+++ 3.longest-substring-without-repeating-characters.cpp
@@ -8,7 +8,27 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        
+        int n = s.size();
+        int left = 0 ;
+        int right = 0;
+
+        int ans = 0;
+
+        vector<int>mp(255,-1);
+        while(right < n)
+        {
+            if(mp[s[right]] != -1 )
+            {
+                left = max(mp[s[right]] + 1 , left );
+            }
+
+            mp[s[right]] = right;
+            ans = max(right - left + 1 , ans);
+            right++;
+        }   
+
+        return ans;
+    
     }
 };
 // @lc code=end
