8478738d9936861db1ba22431153b05e1127017c
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-04-21 23:23:08 +0530

    j

---
+++ 1971.find-if-path-exists-in-graph.cpp
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode id=1971 lang=cpp
+ *
+ * [1971] Find if Path Exists in Graph
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
+        if (n == 1) return true;
+        vector<bool> visited(n, false);
+        visited[source] = true;
+        bool flag = true;
+        while (flag) 
+        {
+            flag = false;
+            for (const auto& edge : edges) 
+            {
+                if (visited[edge[0]] != visited[edge[1]]) 
+                {
+                    visited[edge[0]] = true;
+                    visited[edge[1]] = true;
+                    flag = true;
+                }
+                if (visited[destination]) return true;
+            }
+        }
+        return false;
+    }
+};
+// @lc code=end
+
