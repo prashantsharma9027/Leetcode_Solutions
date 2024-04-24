4b40f21ca9e4fdb971063f0b62accb0c869a9c4a
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-04-24 21:30:25 +0530

    das

---
+++ 1137.n-th-tribonacci-number.cpp
@@ -0,0 +1,21 @@
+/*
+ * @lc app=leetcode id=1137 lang=cpp
+ *
+ * [1137] N-th Tribonacci Number
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int tribonacci(int n) {
+        int dp[] ={0,1,1};
+        for(int i=3;i<=n;i++)
+        {
+            dp[i%3]=dp[0]+dp[1]+dp[2];
+        }
+        
+        return dp[n%3];
+    }
+};
+// @lc code=end
+
