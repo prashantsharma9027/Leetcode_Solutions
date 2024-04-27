7adaea0d57e5bcdb64b2c4d8a7af71115edbc053
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-04-27 21:53:42 +0530

    k5

--- 1137.n-th-tribonacci-number.cpp
+++ 1137.n-th-tribonacci-number.cpp
@@ -10,9 +10,7 @@ public:
     int tribonacci(int n) {
         int dp[] ={0,1,1};
         for(int i=3;i<=n;i++)
-      
-      
-        {
+      {
             dp[i%3]=dp[0]+dp[1]+dp[2];
         }
         
