96ad9766e733012bfcad6a3f8b0d1f1a1a6aa9bc
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-05-13 21:24:14 +0530

    das

---
+++ 861.score-after-flipping-matrix.cpp
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode id=861 lang=cpp
+ *
+ * [861] Score After Flipping Matrix
+ */
+
+// @lc code=start
+#pragma GCC optimize("O3", "unroll-loops")
+class Solution {
+public:
+    int matrixScore(vector<vector<int>>& grid) 
+    {
+        const int r= grid.size(), c= grid[0].size();
+        int sum = 0;
+        for (int j=0; j<c; j++) 
+        {
+            int col1= 0;
+            int b=1<<(c-1-j);
+            for (int i = 0; i < r; i++) 
+            {
+                bool one = (grid[i][0] == 0) ^ (grid[i][j] == 1);
+                col1+=one;
+            }
+            sum += max(r-col1, col1)*b;
+        }
+        return sum;
+    }
+};
+
+// @lc code=end
+
