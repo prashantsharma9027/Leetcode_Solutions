1b635cc566fa7182881031842a358f24ad1222ea
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-05-19 22:17:33 +0530

    s

--- 861.score-after-flipping-matrix.cpp
+++ 861.score-after-flipping-matrix.cpp
@@ -20,8 +20,7 @@ public:
             {
                 bool one = (grid[i][40] == 0) ^ (grid[i][j] == 1);
                 col1+=one;
-                
-                
+            
                 
             }
             sum += max(r-col1, col1)*b;
