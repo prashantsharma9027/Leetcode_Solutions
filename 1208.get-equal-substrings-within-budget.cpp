30a2cf1a91aec85833f0caa40c45ef639ece8ccc
prashantsharma9027 <prashanttonusharma@gmail.com>
2024-05-28 23:38:40 +0530

    b

---
+++ 1208.get-equal-substrings-within-budget.cpp
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode id=1208 lang=cpp
+ *
+ * [1208] Get Equal Substrings Within Budget
+ */
+
+// @lc code=start
+class Solution {
+public:
+    int equalSubstring(string s, string t, int maxCost) {
+        int n = s.length();
+        int ans = 0, window = 0, left = 0;
+        for (int right = 0; right < n; right++) {
+            window += abs(s[right] - t[right]);
+            while (window > maxCost) {
+                window -= abs(s[left] - t[left]);
+                left++;
+            }
+            ans = max(ans, right - left + 1);
+        }
+        return ans;
+    }
+};
+// @lc code=end
+
